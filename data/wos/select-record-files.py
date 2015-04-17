3a9210b3b1dbea5c897e04bd689f7a8af4c29cad
Marc Verhagen <marc@cs.brandeis.edu>
2015-04-16 23:59:21 -0400

    Added code to select WoS records given a subject.

---
+++ data/wos/select-record-files.py
@@ -0,0 +1,88 @@
+"""
+
+Select records from WoS archives given a list of identifiers for some subjects.
+
+Builds a cropus in co
+
+"""
+
+
+import sys, os, gzip, codecs, time
+from utils import WOSItem
+
+WOS_DIR = '/home/j/corpuswork/fuse/FUSEData/2013-04/WoS_2012_Aug'
+
+SUBJECTS_DIR = 'subject-lists'
+
+A01_DIR = 'corpora/A01'
+A04_DIR = 'corpora/A04'
+A10_DIR = 'corpora/A10'
+
+
+
+def select(year, fname):
+
+    print year, fname
+
+    A01, A04, A10 = read_subjects(year, fname)
+    archive = os.path.join(WOS_DIR, fname[:-4] + '.gz')
+    gzipfile = gzip.open(os.path.join(WOS_DIR, archive), 'rb')
+    fh = codecs.getreader('utf-8')(gzipfile)
+
+    A01_subdir = os.path.join(A01_DIR, year, fname[:-4])
+    if not os.path.exists(A01_subdir): os.makedirs(A01_subdir)
+
+    A04_subdir = os.path.join(A04_DIR, year, fname[:-4])
+    if not os.path.exists(A04_subdir): os.makedirs(A04_subdir)
+    
+    A10_subdir = os.path.join(A10_DIR, year, fname[:-4])
+    if not os.path.exists(A10_subdir): os.makedirs(A10_subdir)
+
+    recs = 0
+    rec = []
+    ut = None
+    isA01, isA04, isA10 = False, False, False
+    for line in fh:
+        if line.startswith('<REC>'):
+            recs += 1
+            rec = []
+            ut = None
+            isA01, isA04, isA10 = False, False, False
+        elif line.startswith('<ut>'):
+            rec.append(line)
+            ut = line.split('>')[1].split('<')[0]
+            if ut in A01: isA01 = True
+            elif ut in A04: isA04 = True
+            elif ut in A10: isA10 = True
+        elif line.startswith('</REC>'):
+            if isA01: create_file(A01_subdir, ut, rec)
+            elif isA04: create_file(A04_subdir, ut, rec)
+            elif isA10: create_file(A10_subdir, ut, rec)
+        else:
+            rec.append(line)
+            #if recs > 10: exit()
+
+
+def read_subjects(year, fname):
+    subjects_list = os.path.join(SUBJECTS_DIR, year, fname)
+    A01 = {}
+    A04 = {}
+    A10 = {}
+    for line in open(subjects_list):
+        subj, ut = line.split()
+        if subj == 'A01': A01[ut] = True
+        elif subj == 'A04': A04[ut] = True
+        elif subj == 'A10': A10[ut] = True
+    return A01, A04, A10
+
+def create_file(dir, ut, rec):
+    fh = codecs.open(os.path.join(dir, ut + '.xml'), 'w', encoding='utf8')
+    fh.write("<REC>\n%s</REC>\n" % ''.join(rec))
+
+
+
+if __name__ == '__main__':
+
+    year = sys.argv[1]
+    for fname in sorted(os.listdir(os.path.join(SUBJECTS_DIR, year))):
+        select(year, fname)
---
+++ data/wos/select-record-ids.py
@@ -0,0 +1,48 @@
+"""
+
+Select identifers for a couple of subject domains
+
+Input is in INDEX_DIR, in directories for each year.
+Output is in SUBJECTS_DIR, with same structure as INDEX_DIR.
+
+Creates files with subject groups (A01, A04 and A10) and <ut> identifiers.
+
+"""
+
+import sys, os, glob, gzip, codecs, time
+
+INDEX_DIR = 'index'
+SUBJECTS_DIR = 'subject-lists'
+
+# These are substrings manuallt derived from looking at the subject domains and
+# all available subjects. If a subject matches one of the strings than the
+# subject is part of the group Axx. There seems to be no good option for A06,
+# Nanotechnology.
+A01 = ['MULTIDISCIPLINARY']
+A04 = ['PHYSICS,']
+A10 = ['BIOCHEMISTRY', 'GENETICS', 'BIOTECHNOLOGY', 'CELL BIOLOGY']
+
+
+
+if __name__ == '__main__':
+
+    for year in sorted(os.listdir(INDEX_DIR)):
+        print year
+        if not os.path.exists(os.path.join(SUBJECTS_DIR, year)):
+            os.makedirs(os.path.join(SUBJECTS_DIR, year))
+        fnames = glob.glob(os.path.join(INDEX_DIR, year) + os.sep + 'index-main-*')
+        for fname in fnames:
+            print '  ', fname
+            outfname = os.path.basename(fname)[11:]
+            out = open(os.path.join(SUBJECTS_DIR, year, outfname), 'w')
+            for line in open(fname):
+                fields = line.split("\t")
+                ut = fields[0]
+                subject = fields[5]
+                for t in A01:
+                    if subject.find(t) > -1: out.write("A01 %s\n" % ut)
+                for t in A04:
+                    if subject.find(t) > -1: out.write("A04 %s\n" % ut)
+                for t in A10:
+                    if subject.find(t) > -1: out.write("A10 %s\n" % ut)
+
